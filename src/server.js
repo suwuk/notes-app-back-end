7f6d1e9491b220c5863fb55f4f94f4573769b7bd
suwuk <diaz.yudha29@gmail.com>
2024-08-21 20:05:39 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -18,6 +18,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const ClientError = require('./exceptions/ClientError');
 require('dotenv').config();
 
@@ -91,6 +96,13 @@ const init = async () => {
         validator: CollaborationsValidator,
       },
     },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
+      },
+    },    
   ]);
 
   server.ext('onPreResponse', (request, h) => {
